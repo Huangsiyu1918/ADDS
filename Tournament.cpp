eafea3779a860e9be55204e0e33a566c826670ff
Huangsiyu1918 <64762887+Huangsiyu1918@users.noreply.github.com>
2021-03-17 12:29:42 +1030

    update a3-1

--- Tournament.cpp
+++ Tournament.cpp
@@ -12,8 +12,7 @@ Player* Tournament::run(std::array<Player *, 8> competitors)
 
     //std::array<Player*, 4> round;
     std::vector<Player*> res1;
-    int k = 0;
-    cout << "T1: " << endl;
+    // cout << "T1: " << endl;
     for (int i = 0; i < n; i+=2)
     {
         Player* temp;
@@ -24,19 +23,16 @@ Player* Tournament::run(std::array<Player *, 8> competitors)
     }
 
     // n = n/2;
-    // run(std::array<Player *, n> round);
      std::vector<Player*> res2;
-    cout << "T2: " << endl;
+    // cout << "T2: " << endl;
     for (int i = 0; i < res1.size(); i+=2)
     {
         Player* temp;
         temp = ref.refGame(res1[i], res1[i+1]);
-        //round[k] = temp;
-        //k++;
         res2.push_back(temp);
     }
 
-    cout << "T3: " << endl;
+    // cout << "T3: " << endl;
     return ref.refGame(res2[0], res2[1]);
     
 }
